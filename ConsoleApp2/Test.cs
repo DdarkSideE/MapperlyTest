627d853cb9b1709a3dc73df3f52f4979df0c6f48
Andrei Gavrilov <ddarksidee@gmail.com>
2024-03-20 12:24:56 +0100

    first commit

---
+++ ConsoleApp2/Test.cs
@@ -0,0 +1,17 @@
+﻿using Riok.Mapperly.Abstractions;
+
+class Fruit {}
+class Banana : Fruit {}
+class Apple : Fruit {}
+
+class BananaDto {}
+class AppleDto {}
+
+[Mapper]
+partial class Mapper
+{
+    public static partial TTarget MapFruit<TTarget>(Fruit source);
+
+    private static partial BananaDto MapBanana(Banana source);
+    private static partial AppleDto MapApple(Apple source);
+}
